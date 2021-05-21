2a80054c087c1cce5b339f515f26874702346241
LuK1337 <priv.luk@gmail.com>
2021-05-21 15:32:47 +0200

    stanford: overlay: Set default Settings.Global.DEVICE_NAME according to EMUI
    EMUI uses ro.config.marketing_name for default device name.
    
    Change-Id: I310c1ffc581b72653783d1093d9e7ebde544bb16

--- device.mk
+++ device.mk
@@ -14,6 +14,10 @@ TARGET_SCREEN_WIDTH := 1080
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 24
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
