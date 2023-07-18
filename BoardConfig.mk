ae91be764356d9199dafeb46d78c89d5da871dd8
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-18 11:48:10 +0200

    stanford: Define camera platform
    Change-Id: Ic71685cc5ee453b60e5562dd0f8eea68d568a09b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,8 @@ include device/huawei/hi3660-common/BoardConfigCommon.mk
 # Display
 TARGET_SCREEN_DENSITY := 480
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Inherit the proprietary files
 include vendor/huawei/stanford/BoardConfigVendor.mk
