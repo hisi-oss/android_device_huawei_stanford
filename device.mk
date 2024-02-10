b90b1e45b57ec1d20b1bd58245c600c2388bb4ad
Giovanni Ricca <giovanniricca@proton.me>
2024-02-10 15:56:11 +0100

    stanford: Set bootanimation resolution
    Change-Id: Ifa2460444fffb2a621556af3b27eae63f5836718

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Inherit from hi3660-common
 $(call inherit-product, device/huawei/hi3660-common/common.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 24
 
