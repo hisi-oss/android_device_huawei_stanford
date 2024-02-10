4981c8fa330bdf8b8ad22a8e0576ad70efff3d8d
Giovanni Ricca <giovanniricca@proton.me>
2024-02-10 15:54:30 +0100

    stanford: Set shipping api level
    Change-Id: I3a9b0a84df51e079c4c96690279f3f48466c5bc3

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Inherit from hi3660-common
 $(call inherit-product, device/huawei/hi3660-common/common.mk)
 
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 24
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
