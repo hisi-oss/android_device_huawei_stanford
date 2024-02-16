ea4159e73be857571fc8691ca02d7b96f6b5b3e8
Giovanni Ricca <giovanniricca@proton.me>
2024-02-16 10:17:47 +0100

    stanford: Import extract-utils scripts
    * HEAD to
      https://github.com/LineageOS/android_tools_extract-utils/tree/9724a2a4105fd396dc7e84fac3b2bddec0e77f73/templates/multi-device/device
    
    Change-Id: I81748b54df0d9fc09a2f13aa95e5fb2819108e9d

---
+++ extract-files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=stanford
+export DEVICE_COMMON=hi3660-common
+export VENDOR=huawei
+export VENDOR_COMMON=${VENDOR}
+
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
